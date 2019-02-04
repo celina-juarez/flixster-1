c89b547a44029605e25dfeb219940d09e2296c6e
Celina Juarez <cejuarez@csumb.edu>
2019-02-04 14:52:19 -0800

    Added details page

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Celina on 2/4/19.
+//  Copyright © 2019 Celina. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print(movie["title"])
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
